ad29ef3c93f19384a253504fb99aa621c4fca1ed
Steven Hilton <mshiltonj@gmail.com>
2012-08-30 13:24:59 -0400

    getting some tests and coverage

--- lib/benny_cache/cache.rb
+++ lib/benny_cache/cache.rb
@@ -1,5 +1,35 @@
 module BennyCache
   class Cache
+    def initialize 
+      @cache = {}
+    end
+
+    def fetch(key, &block)
+      val = @cache[key]
+
+      if val.nil? && block_given?
+        val = block.call()
+        @cache[key] = val
+      end
+      val
+    end
+
+    def read(key)
+      @cache[key]
+    end
+
+    def write(key, val)
+      @cache[key] = val
+      return true
+    end
+
+    def delete(key)
+      @cache.delete(key)
+    end
+
+    def clear
+      @cache = {}
+    end
 
   end
 end
--- spec/models/cache_spec.rb
+++ spec/models/cache_spec.rb
@@ -3,4 +3,57 @@ describe BennyCache::Cache do
   it "is a class" do
     BennyCache::Cache
   end
+
+  it "can be instantiated" do
+    c = BennyCache::Cache.new
+    c.should be_true
+  end
+
+  describe do 
+    before(:each) do
+      @c = BennyCache::Cache.new
+      @key = "foo"
+      @val = "bar"
+    end
+
+    methods = %w(fetch read write delete)
+    methods.each do |m|
+      it "should respond to #{m}" do
+        @c.respond_to?(m).should be_true    
+      end
+    end
+
+    it "should write and read keyed data" do
+      @c.write(@key, @val)
+      @c.read(@key).should == @val
+    end
+
+    it "can delete keys" do
+      @c.write(@key, @val)
+      @c.read(@key).should == @val
+      @c.delete(@key)
+      @c.read(@key).should be_nil
+    end
+
+    it "#fetch can set a nil val with a block" do
+      @c.read(@key).should be_nil
+      set_val = @c.fetch(@key) {
+        @val
+      }
+      set_val.should == @val 
+      @c.read(@key).should == @val
+    end
+
+    it "#clear should remove all cached data" do
+      key2 = "baz"
+      val2 = "bin"
+      @c.write(@key, @val)
+      @c.write(key2, val2)
+      @c.read(@key).should == @val
+      @c.read(key2).should == val2
+      @c.clear
+      @c.read(@key).should be_nil
+      @c.read(key2).should be_nil
+    end
+  end
 end
