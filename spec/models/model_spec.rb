9df23f75e356d1bc137a61641d1285b1e881746d
Steven Hilton <mshiltonj@gmail.com>
2012-08-30 16:13:32 -0400

    more testing

---
+++ spec/models/model_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../spec_helper"
+require_relative "../test_classes"
+
+
+describe BennyCache::Model do
+  it "should exist" do
+    BennyCache::Model.should be_true
+  end
+end
---
+++ spec/test_classes.rb
@@ -0,0 +1,49 @@
+class ARFaker
+
+  def save
+    self.class_variable_get(:@@after_save_callbacks).each do |cb|
+      self.send(cb)
+    end
+  end
+
+  def destroy
+    self.class_variable_get(:@@after_destroy_callbacks).each do |cb|
+      self.send(cb)
+    end
+  end
+
+  def self.after_save(*methods)
+    unless self.class_variable_defined?(:@@after_save_callbacks)
+      self.class_variable_set(:@@after_save_callbacks, [])
+    end
+    self.class_variable_get(:@@after_save_callbacks).push(*methods)
+  end
+
+  def self.after_destroy(*methods)
+    unless self.class_variable_defined?(:@@after_destroy_callbacks)
+      self.class_variable_set(:@@after_destroy_callbacks, [])
+    end
+
+    self.class_variable_get(:@@after_destroy_callbacks).push(*methods)
+  end
+
+end
+
+
+class ModelCacheFake < ARFaker
+  include BennyCache::Model
+
+  benny_model_index :other_id, [:x, :y]
+  benny_data_index :stuff
+
+end
+
+class RelatedCacheFake < ARFaker
+  include BennyCache::Related
+  benny_related_index "ModelCacheFake/:model_id/stuff"
+
+  attr_accessor :model_id
+
+end
+
+
